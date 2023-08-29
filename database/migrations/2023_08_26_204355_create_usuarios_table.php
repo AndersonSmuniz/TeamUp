0551106b0fdd7faa94833e9bcdd7bd71e5c01b58
AndersonSmuniz <anderson.muniz100@hotmail.com>
2023-08-29 01:24:54 -0300

    criação das migrations tipo_usuario e usuario

---
+++ database/migrations/2023_08_26_204355_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('apelido')->unique();
+            $table->string('email')->unique();
+            $table->string('senha');
+            $table->rememberToken();
+            $table->string('celular')->nullable();
+            $table->foreignId('tipo_usuario_id')->constrained('tipo_usuarios');
+            $table->string('midia_social')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};

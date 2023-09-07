0c7453f0fcde44df2a03f1ed0506358b7cbe0f68
AndersonSmuniz <anderson.muniz100@hotmail.com>
2023-09-07 14:02:01 -0300

    Modificação nas configs de auth, Criação de novas rotas em api.php

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,33 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\UsuarioController;
+use \App\Http\Controllers\Api\V1\EsporteController;
+use \App\Http\Controllers\Api\V1\TipoPontuacaoController;
+use \App\Http\Controllers\AuthController;
 
 
 Route::prefix('V1')->group(function (){
 
-    Route::apiResource('usuarios', UsuarioController::class);
+    Route::post('login', [AuthController::class, 'login']);
 
+    Route::post('usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::get('usuarios', [UsuarioController::class, 'index'])->name('usuarios.store');
+        Route::post('usuarios/{id}', [UsuarioController::class, 'show'])->name('usuarios.store');
+        Route::put('usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.store');
+        Route::delete('usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuarios.store');
+        Route::post('logout', [AuthController::class, 'logout']);
+
+        Route::apiResource('esportes', EsporteController::class);
+
+        Route::get('esportes/{esporte}/tipo_pontuacao', [TipoPontuacaoController::class, 'index'])->name('tipo_pontuacao.index');
+        Route::post('esportes/{esporte}/tipo_pontuacao', [TipoPontuacaoController::class, 'store'])->name('tipo_pontuacao.store');
+        Route::post('esportes/{esporte}/tipo_pontuacao/{id}', [TipoPontuacaoController::class, 'show'])->name('tipo_pontuacao.show');
+        Route::put('esportes/{esporte}/tipo_pontuacao/{id}', [TipoPontuacaoController::class, 'update'])->name('tipo_pontuacao.update');
+        Route::delete('esportes/{esporte}/tipo_pontuacao/{id}', [TipoPontuacaoController::class, 'destroy'])->name('tipo_pontuacao.destroy');
+
+    });
 });
 

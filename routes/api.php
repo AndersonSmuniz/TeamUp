d9169697e03e27c2ff410ca441e50b3a09adff6a
AndersonSmuniz <anderson.muniz100@hotmail.com>
2023-09-11 12:15:26 -0300

    Criação de novas rotas

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use \App\Http\Controllers\Api\V1\UsuarioController;
 use \App\Http\Controllers\Api\V1\EsporteController;
 use \App\Http\Controllers\Api\V1\TipoPontuacaoController;
 use \App\Http\Controllers\AuthController;
+use \App\Http\Controllers\Api\V1\CartaController;
+use \App\Http\Controllers\Api\V1\PartidaController;
 
 
 Route::prefix('V1')->group(function (){
@@ -16,10 +18,11 @@ Route::prefix('V1')->group(function (){
 
     Route::middleware('auth:sanctum')->group(function () {
 
-        Route::get('usuarios', [UsuarioController::class, 'index'])->name('usuarios.store');
-        Route::post('usuarios/{id}', [UsuarioController::class, 'show'])->name('usuarios.store');
-        Route::put('usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.store');
-        Route::delete('usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuarios.store');
+        Route::get('usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
+        Route::get('usuarios/{id}', [UsuarioController::class, 'show'])->name('usuarios.show');
+        Route::put('usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.update');
+        Route::delete('usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuarios.destroy');
+
         Route::post('logout', [AuthController::class, 'logout']);
 
         Route::apiResource('esportes', EsporteController::class);
@@ -29,6 +32,13 @@ Route::prefix('V1')->group(function (){
         Route::post('esportes/{esporte}/tipo_pontuacao/{id}', [TipoPontuacaoController::class, 'show'])->name('tipo_pontuacao.show');
         Route::put('esportes/{esporte}/tipo_pontuacao/{id}', [TipoPontuacaoController::class, 'update'])->name('tipo_pontuacao.update');
         Route::delete('esportes/{esporte}/tipo_pontuacao/{id}', [TipoPontuacaoController::class, 'destroy'])->name('tipo_pontuacao.destroy');
+
+        Route::post('carta', [CartaController::class, 'CriarCarta'])->name('carta.CriarCarta');
+        Route::get('carta', [CartaController::class, 'index'])->name('carta.index');
+        Route::delete('carta/{id}', [CartaController::class, 'destroy'])->name('carta.destroy');
+
+        Route::apiResource('partida', PartidaController::class);
+        Route::post('partida/{partida}/entrar', [PartidaController::class,'entrar'])->name('partida.entrar');
 
     });
 });

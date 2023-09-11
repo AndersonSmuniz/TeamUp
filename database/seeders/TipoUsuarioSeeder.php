cad3cb34cce243d1ec0bd6877edf454c60cc93d3
AndersonSmuniz <anderson.muniz100@hotmail.com>
2023-09-11 12:11:54 -0300

    Criação de seeds para o banco de dados

--- database/seeders/TipoUsuarioSeeder.php
+++ database/seeders/TipoUsuarioSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class TipoUsuarioSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
         DB::table('tipo_usuarios')->insert([
             ['nome' => 'Administrador'],
